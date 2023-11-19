2a8bef702fb780d615ad3c5d9472e890ac05a661
Eduardo Aguiar <eduardo.poerner@catolicasc.edu.br>
2023-11-19 20:24:56 -0300

    Config

--- src/pages/api/[...slug].js
+++
@@ -1,9 +0,0 @@
-import httpProxyMiddleware from 'next-http-proxy-middleware'
-import {BASE_URL_API} from "../../services/api";
-
-export default (req, res) => httpProxyMiddleware(req, res, {
-    target: `${process.env.LOCAL_SERVER_URL || BASE_URL_API}`,
-    pathRewrite: {
-        '^/api/': ''
-    }
-})
